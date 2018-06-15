3ef7bec08b583c9c2ef3e2e40822ae05392c4a4b
shovelyjoe <rob.oc@hotmail.com>
2018-06-15 17:14:41 +0100

    Step 16 - One Spare test, add score in frames not per roll. Pass)

--- 10-pin-bowling/bowlingGame.js
+++ 10-pin-bowling/bowlingGame.js
@@ -6,10 +6,11 @@ const game = () => {
   return {
     getScore: () => {
       let score = 0;
-      for(let i = 0; i < rolls.length; i++)
-        if(rolls[i] + rolls[i+1] === 10) //Spare
-          score += //Design still wrong - need to check as frames, not two rolls
-        score += rolls[i];
+      let i = 0;
+      for(let frame = 0; frame < 10; frame++){
+        score += rolls[i] + rolls[i+1];
+        i += 2;
+      }
       return score;
     },
     roll: (pins) => {
