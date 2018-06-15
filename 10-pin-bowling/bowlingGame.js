8b15df796212f61e8c411fee542870f40896e0cc
shovelyjoe <rob.oc@hotmail.com>
2018-06-15 17:41:15 +0100

    Step 21 - One Strike test, write code to handle Strike + bonus; note untidiness for later refactor. Pass

--- 10-pin-bowling/bowlingGame.js
+++ 10-pin-bowling/bowlingGame.js
@@ -10,7 +10,10 @@ const game = () => {
       let score = 0;
       let frameIndex = 0;
       for(let frame = 0; frame < 10; frame++){
-        if(isSpare(frameIndex)){
+        if(rolls[frameIndex] === 10){//Strike
+          score += 10 + rolls[frameIndex+1] + rolls[frameIndex+2];
+          frameIndex++;
+        }else if(isSpare(frameIndex)){
           score += 10 + rolls[frameIndex+2];
           frameIndex += 2;
         }else{
