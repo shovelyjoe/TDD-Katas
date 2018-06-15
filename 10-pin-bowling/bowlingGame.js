1ad9b729c6b9762a05af22abcc28d9ca66ae17bc
shovelyjoe <rob.oc@hotmail.com>
2018-06-15 16:54:07 +0100

    Step 11 - One Spare test, refactor rolls() to keep game roll history. Pass)

--- 10-pin-bowling/bowlingGame.js
+++ 10-pin-bowling/bowlingGame.js
@@ -1,8 +1,14 @@
 const game = () => {
   let score = 0;
+  let rolls = new Array(21);
+  let currentRoll = 0;
+
   return {
     getScore: () => score,
-    roll: (pins) => score += pins
+    roll: (pins) => {
+      score += pins;
+      rolls[currentRoll++] = pins;
+    }
   }
 }
 
