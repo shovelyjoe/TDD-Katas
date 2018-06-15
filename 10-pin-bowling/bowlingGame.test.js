55b556f2d58c352d55b2781ff69ee242c6453fe1
shovelyjoe <rob.oc@hotmail.com>
2018-06-15 10:44:56 +0100

    Step 6 - All Ones test, write test for 20xOnes; note duplicates for later refactor, expect score = 20. Fail (Expected: 20, Received: 0)

--- 10-pin-bowling/bowlingGame.test.js
+++ 10-pin-bowling/bowlingGame.test.js
@@ -6,3 +6,9 @@ test('Gutter Game', () => {
   expect(game.score()).toBe(0);
 });
 
+test('All Ones', () => {
+  for(let i = 0; i < 20; i++)//Duplicated for loop
+    game.roll(1);
+  expect(game.score()).toBe(20);
+});
+
